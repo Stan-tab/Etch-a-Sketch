86c21876fb427c02b53b4541d825c1fe1eacc63a
Stan-tab <godofwarmafia2@gmail.com>
2024-11-26 19:07:36 +0500

    Add Grid

--- somejs.js
+++ somejs.js
@@ -6,14 +6,25 @@ let userInput = 16;
 button.textContent = "Change value";
 body.insertBefore(button, body.firstChild);
 
+adder(16)
+
 button.addEventListener("click", () => {
     const previousNumber = userInput;
     userInput = parseInt(prompt("Enter the value of the scetch size\ntill 100"));
-    if (isNaN(userInput)) {
+    if (isNaN(userInput) || userInput > 100) {
         userInput = previousNumber;
     }
+    adder(userInput);
 })
 
-for (let i = 0; i<userInput**2; i++){
-    mainBox.appendChild(document.createElement("div"));
+function adder(num) {
+    mainBox.replaceChildren("");
+    for (let i = 0; i<num; i++){
+        const yAciss = document.createElement("div");
+        yAciss.classList.add("vertical");
+        for (let l = 0; l < num; l++) {
+            yAciss.appendChild(document.createElement("div"));
+        }
+        mainBox.appendChild(yAciss)
+    }
 }
